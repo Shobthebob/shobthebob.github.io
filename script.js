d31a1345bc3448d572191cdd8a0cf2eb79551833
Shoubhit <Shobthebob@users.noreply.github.com>
2024-08-07 01:46:31 -0400

    Done with Hero section (for now atleast)

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const fullTextElement = document.querySelector('.first-half')
     const textElement = document.querySelector('.shob-jam');
-    const words = ["Shoubhit Jamadhiar", "a student", "a developer", ""];
+    const fullText = fullTextElement.textContent;
+    const words = ["Shoubhit Jamadhiar", "a student", "a developer"];
     let wordIndex = 0;
     let letterIndex = 0;
+    let letterIndex1 = 0;
     let currentWord = words[wordIndex];
     let isDeleting = false;
-    const typingSpeed = 100;
+    const typingSpeed = 95;
     const erasingSpeed = 60;
     const delayBetweenWords = 2000;
+
+    function typeFullText() {
+        console.log(fullText);
+        if (letterIndex1 < fullText.length) {
+            fullTextElement.textContent = fullText.substring(0, letterIndex1 + 1);
+            letterIndex1++;
+            setTimeout(typeFullText, typingSpeed);
+        }
+        else {
+            type()
+        }
+    }
 
     function type() {
         if (isDeleting) {
@@ -21,7 +36,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 currentWord = words[wordIndex];
                 setTimeout(type, typingSpeed + 500); // Adding a slight delay before starting to type the new word
             }
-        } else {
+        }
+        else {
             if (letterIndex < currentWord.length) {
                 textElement.textContent = currentWord.substring(0, letterIndex + 1);
                 letterIndex++;
@@ -32,7 +48,49 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
+    
+    typeFullText();
 
-    textElement.classList.add('typing');
-    type();
+
+    const tiltContainer = document.querySelector('.tilt-container');
+    const shine = document.querySelector('.shine');
+    
+    let bounds;
+
+    function rotateToMouse(e) {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        const leftX = mouseX - bounds.x;
+        const topY = mouseY - bounds.y;
+        const center = {
+        x: leftX - bounds.width / 2,
+        y: topY - bounds.height / 2
+        }
+        const distance = Math.sqrt(center.x**2 + center.y**2);
+        
+        tiltContainer.style.transform = `
+        scale3d(1.07, 1.07, 1.07)
+        rotate3d(
+            ${center.y / 100},
+            ${-center.x / 100},
+            0,
+            ${Math.log(distance) * 2}deg
+        )
+        `;
+        
+        shine.style.opacity = 0.5;
+        shine.style.transform = `translate3d(${center.x / 4}px, ${center.y / 4}px, 0)`;
+    }
+
+    tiltContainer.addEventListener('mouseenter', () => {
+        bounds = tiltContainer.getBoundingClientRect();
+        document.addEventListener('mousemove', rotateToMouse);
+    });
+
+    tiltContainer.addEventListener('mouseleave', () => {
+        document.removeEventListener('mousemove', rotateToMouse);
+        tiltContainer.style.transform = '';
+        tiltContainer.style.background = '';
+        shine.style.opacity = 0;
+    });
 });
